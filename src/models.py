6edde66695c3bc553280835525e44311532376b0
David Barreiro <86165842+david-barrei@users.noreply.github.com>
2025-03-10 04:11:13 +0000

    terminado

--- src/models.py
+++ src/models.py
@@ -6,28 +6,55 @@ from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
+class Pokedex(Base):
+    __tablename__ = 'pokedex'
+
     id: Mapped[int] = mapped_column(primary_key=True)
-    name: Mapped[str] = mapped_column(nullable=False)
-    address: Mapped["Address"] = relationship(back_populates="person")
+    nombre: Mapped[str] = mapped_column()
+    numero: Mapped[int] = mapped_column()
+    tipo: Mapped[str] = mapped_column()
 
+class Evolucion(Base):
+    __tablename__ = 'evolucion'
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
     id: Mapped[int] = mapped_column(primary_key=True)
-    street_name: Mapped[str]
-    street_number: Mapped[str]
-    post_code: Mapped[str] = mapped_column(nullable=False)
-    person_id: Mapped[int] = mapped_column(ForeignKey("person.id"))
-    person: Mapped["Person"] = relationship(back_populates="address")
+    nameEvo: Mapped[str] = mapped_column()
+    evolucion1: Mapped[str] = mapped_column()
+    evolucion2: Mapped[str] = mapped_column()
+    evolucion3: Mapped[str] = mapped_column()
+    pokemon_id:Mapped[int]=mapped_column(ForeignKey('pokedex.id'))
 
-    def to_dict(self):
-        return {}
+class Ataque(Base):
+    __tablename__ = 'ataque'
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    defensa:Mapped[str] = mapped_column()
+    velocidad: Mapped[int] = mapped_column()
+    ataque_especial:Mapped[str] = mapped_column()
+    poke_id: Mapped[int]=mapped_column(ForeignKey('pokedex.id'))
+    evolucion: Mapped[int]=mapped_column(ForeignKey('evolucion.id'))
+
+class Debilidad(Base):
+    __tablename__= 'debilidad'
+
+    id:Mapped[int] = mapped_column(primary_key=True)
+    devilidad1:Mapped[str] = mapped_column()
+    devilidad2:Mapped[str] = mapped_column()
+    devilidad3:Mapped[str] = mapped_column()
+
+class Caracteristica(Base):
+    __tablename__ = 'caracteristica'
+
+    id: Mapped[int]=mapped_column(primary_key=True)
+    altura: Mapped[int] = mapped_column()
+    peso:Mapped[int] = mapped_column()
+    sexo: Mapped[str] = mapped_column()
+    habilidad:Mapped[str] = mapped_column()
+    devilidad:Mapped[int] = mapped_column(ForeignKey('debilidad.id'))
+    pokemon:Mapped[int] = mapped_column(ForeignKey('pokedex.id'))
+    evo_id:Mapped[int] = mapped_column(ForeignKey('evolucion.id'))
+
+
 
 ## Draw from SQLAlchemy base
 render_er(Base, 'diagram.png')
